c52cc453eb0842ebedc3c84f74b7702f6e285e8a
preciouStar <seongjin605@users.noreply.github.com>
2021-01-25 21:13:10 +0900

    Add solution

--- leetcode/3.hard/trapping-rain-water.py
+++ leetcode/3.hard/trapping-rain-water.py
@@ -3,34 +3,34 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len( height )
-        if n < 3:
-            return 0
-        left = 0
-        right = n - 1
-        left_max = height[0]
-        right_max = height[n - 1]
-        output = 0
+        left, right = 0, len( height ) - 1
+
+        left_max = 0
+        right_max = height[right]
+
+        result = 0
+
         while left <= right:
-            if left_max < right_max:
-                output += left_max - height[left]
+            if left_max <= right_max:
+                result += left_max - height[left]
                 left += 1
-                if left == n:
+                if left == len( height ):
                     break
                 left_max = max( left_max, height[left] )
             else:
-                output += right_max - height[right]
+                result += right_max - height[right]
                 right -= 1
-                if right == -1:
+                if right == 0:
                     break
                 right_max = max( right_max, height[right] )
-            return output
 
-        '''
-        height = [0,1,0,2,1,0,1,3,2,1,2,1]
-                            L
-                                R
-        left_max = 2
-        right_max = 3
-        output = 6
-        '''
+        return result
+
+    '''
+    height = [0,1,0,2,1,0,1,3,2,1,2,1]
+                        L
+                            R
+    left_max = 2
+    right_max = 3
+    output = 6
+    '''
