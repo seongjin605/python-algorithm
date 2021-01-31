c6310d32e98b0fc888170c0da279a82f441d1c89
preciouStar <seongjin605@users.noreply.github.com>
2021-01-31 20:41:21 +0900

    solve problem

--- leetcode/2.medium/product-of-array-except-self.py
+++ leetcode/2.medium/product-of-array-except-self.py
@@ -1,19 +1,28 @@
 from functools import reduce
 
+
 # https://leetcode.com/problems/product-of-array-except-self/submissions/
 def productExceptSelf():
-    # nums = [1, 2, 3, 4]
-    nums = [9, 0, -2]
-    # has_zero = filter( lambda x: x == 0, nums )
-    # if has_zero == None:
+    nums = [1, 2, 3, 4]
+    # nums = [9, 0, -2] => [0,-18,0]
+    # nums = [0, 0]
 
-    result = []
-    for index, value in enumerate( nums ):
-        # 자신을 제외한 배열을 곱한다.
-        filtered_nums = filter( lambda x: x != value, nums )
-        reduced_num = reduce( lambda x, y: x * y, filtered_nums, 1 )
-        result.append( reduced_num )
-    return result
+    out = []
+    p = 1
+    # 왼쪽 곱셈
+    for i in range( 0, len( nums ) ):
+        out.append( p )
+        p = p * nums[i]
+
+    print( "p:", p )
+    print( "output:", out )
+    p = 1
+    # 왼쪽 곱셈 결과에 오른쪽 값을 차례대로 곱셈
+    for i in range( len( nums ) - 1, -1, -1 ):
+        print( "i:", i )
+        out[i] = out[i] * p
+        p = p * nums[i]
+    return out
 
 
 r = productExceptSelf()
