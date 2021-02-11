a1dde48a785ab0c57832b3b6fe0f9af28a7d18a1
preciouStar <seongjin605@users.noreply.github.com>
2021-02-11 21:21:28 +0900

    solve problem

--- leetcode/1.easy/reverse-linked-list.py
+++ leetcode/1.easy/reverse-linked-list.py
@@ -8,10 +8,18 @@ class ListNode:
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
         # recursive
-        def reverse(node: ListNode, prev: ListNode = None):
-            if not node:
-                return prev
-            next, node.next = node.next, prev
-            return reverse( next, node )
+        # def reverse(node: ListNode, prev: ListNode = None):
+        #     if not node:
+        #         return prev
+        #     next, node.next = node.next, prev
+        #     return reverse( next, node )
+        #
+        # return reverse( head )
 
-        return reverse( head )
+        # Iterative
+        node, prev = head, None
+
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node.next, next
+        return prev
