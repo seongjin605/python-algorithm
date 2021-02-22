ffbf13f900dcfcb97c5c9f32ba0e3d9c29b31536
preciouStar <seongjin605@users.noreply.github.com>
2021-02-23 00:13:52 +0900

    Solve problem

--- leetcode/2.medium/binary-tree-level-order-traversal.py
+++ leetcode/2.medium/binary-tree-level-order-traversal.py
@@ -1,25 +1,26 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-from typing import List
+import collections
 
-# https://leetcode.com/problems/binary-tree-level-order-traversal/
-def levelOrder(self, root: TreeNode) -> List[List[int]]:
-    if root == None: return []
-    treeNodes, result = [], []
-    treeNodes.append(root)
-    while len(treeNodes) > 0:
-        current = []
-        size = len(treeNodes)
-        for i in range(0, size):
-            node = treeNodes.pop(0)
-            if node != None and node.left != None:
-                treeNodes.append(node.left)
-            if node != None and node.right != None:
-                treeNodes.append(node.right)
-            current.append(node.val)
-        result.append(current)
-    return result
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        tree_nodes = collections.deque([root])
+        result = []
+        while len(tree_nodes) > 0:
+            current = collections.deque([])
+            tree_size = len(tree_nodes)
+            for i in range(tree_size):
+                node = tree_nodes.popleft()
+                if node.left:
+                    tree_nodes.append(node.left)
+                if node.right:
+                    tree_nodes.append(node.right)
+                current.append(node.val)
+            result.append(current)
+        return result
